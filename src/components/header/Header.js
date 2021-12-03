2ba19671c6a43e027ea0fd35a69b3258b55a6fd3
Alexey Kovalyuk <nehochubanan@gmail.com>
2021-12-03 22:36:50 +0800

    Add buttons and progress in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,13 @@
 import React, {useState} from 'react';
 import s from './Header.module.scss';
 import {GlobalSvgSelector} from "../../assets/icons/global/GlobalSvgSelector";
-
+import {Button, LinearProgress} from '@mui/material';
 
 export const Header = () => {
+
+    const data = {
+        progress: 50,
+    }
 
     return (
         <div className={s.root}>
@@ -11,6 +15,13 @@ export const Header = () => {
                 <div className={s.title}>PASCAL</div>
 
                 <div className={s.wrapper}>
+                    <Button sx={{marginRight:'20px'}} variant='outlined'>Адаптационный курс</Button>
+
+                    <div>
+                        <LinearProgress value={data.progress} variant='determinate'
+                        sx={{height: '18px', width: '250px', marginRight: '20px', borderRadius: '9px'}}/>
+                    </div>
+
                     <div
                         className={s.button_profile}
                     >
